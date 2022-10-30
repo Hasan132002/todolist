068448c8358b6d347b8538bc386e269634b55970
Hasan Raza <hasanraza132002@gmail.com>
2022-10-30 19:03:34 +0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+//create route for storing item data
+Route::get('/items',[ItemController::class,'index']);
+Route::prefix('item')->group(function(){
+    Route::post('/store',[ItemController::class,'store']);
+    Route::post('/{id}',[ItemController::class,'update']);
+    Route::post('/{id}',[ItemController::class,'destroy']);
+
+});
